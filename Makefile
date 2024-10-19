96e9c1f6001c66ae674313b5f653a40f3c0fce68
root <root@DESKTOP-AB0P655>
2024-10-19 09:11:15 -0500

    Adjusted Makefile for request class

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 CC = g++
-
 CFLAGS = -Wall -Werror -std=c++23
 
-main: main.o
-	$(CC) $(CFLAGS) -o main.out main.o
+main: main.o request.o
+	$(CC) $(CFLAGS) -o main.out main.o request.o
 
 main.o: main.cpp
 	$(CC) $(CFLAGS) -c main.cpp
 
+request.o: request.cpp
+	$(CC) $(CFLAGS) -c request.cpp
+
 clean:
 	rm -f main.out *.o
-
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-
+#include "request.h"
 
 int main() {
-    std::cout << "Hello, CSCE 412!" << std::endl;
+    Request req("192.168.1.1", "192.168.1.2");
+
+    std::cout << "Request IP in: " << req.getIpIn() << std::endl;
+    std::cout << "Request IP out: " << req.getIpOut() << std::endl;
+
     return 0;
+
 }
